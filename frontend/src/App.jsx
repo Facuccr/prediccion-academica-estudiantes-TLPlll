0f03fb1afc4f1d47cb7fba6cf879f70c8e4bd35b
FacuuC <facundocristaldo146@gmail.com>
2026-06-21 22:27:17 -0300

    cambio de la URL de la API de predicción a la versión desplegada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/predict",
+        "https://prediccion-academica-estudiantes-tlplll.onrender.com",
         formData,
       );
 
